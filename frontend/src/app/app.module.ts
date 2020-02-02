cf7ec6f500f0499d1482a2222bc3dc67f19c391d
Amanda Lin <amandalincs@gmail.com>
2020-02-02 09:55:28 -0800

    working version

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
-import { ChartsModule } from 'ng2-charts'
+// import { ChartsModule } from 'ng2-charts'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,8 +18,7 @@ import { ReflectComponent } from './components/reflect/reflect.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    ChartsModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
