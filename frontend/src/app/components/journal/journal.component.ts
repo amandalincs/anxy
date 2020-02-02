822458a40f4240c0188fec38340d26551aa4f57c
Amanda Lin <amandalincs@gmail.com>
2020-02-02 00:57:41 -0800

    basic ui added

--- frontend/src/app/components/journal/journal.component.ts
+++ frontend/src/app/components/journal/journal.component.ts
@@ -11,4 +11,33 @@ export class JournalComponent implements OnInit {
     constructor(){}
 
     ngOnInit(){}
+
+    submitEntry()
+    {
+        let userFeelings = (<HTMLInputElement>document.getElementById('input1')).value;
+        let userCategory = (<HTMLInputElement>document.getElementById('category1')).value;
+        let userFix = (<HTMLInputElement>document.getElementById('fix1')).value;
+        let userData = {
+            bothering: userFeelings,
+            category: userCategory,
+            goal: userFix
+        };
+
+        console.log(userData);
+
+        // fetch('http://localhost:5000/api/categories/dfxer45tft/',{
+        //     method: 'POST',
+        //     body: JSON.stringify(userData)
+        // }).then(response =>{
+
+        // });
+        
+    }
+
+    addEntryRow()
+    {
+        var itm = document.getElementById("entry1");
+        var cln = itm.cloneNode(true);
+        document.getElementById('day1').appendChild(cln);
+    }
 }
